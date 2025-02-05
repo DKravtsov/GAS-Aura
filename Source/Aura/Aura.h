17a5a3160db7df5d694fcaf30dbd6c552a602a4f
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-02-05 17:16:38 +0200

    highlighting enemies using post-process

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -4,3 +4,4 @@
 
 #include "CoreMinimal.h"
 
+constexpr int32 CUSTOM_DEPTH_STENCIL_RED = 250;
--- Source/Aura/Characters/AuraEnemyCharacter.cpp
+++ Source/Aura/Characters/AuraEnemyCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "Characters/AuraEnemyCharacter.h"
 #include "Components/CapsuleComponent.h"
+#include "Aura.h"
 
 AAuraEnemyCharacter::AAuraEnemyCharacter()
 {
@@ -10,10 +11,26 @@ AAuraEnemyCharacter::AAuraEnemyCharacter()
 
 void AAuraEnemyCharacter::HighlightActor()
 {
-    GetCapsuleComponent()->SetHiddenInGame(false);
+    if (USkeletalMeshComponent* MyMesh = GetMesh())
+    {
+        MyMesh->SetRenderCustomDepth(true);
+        MyMesh->SetCustomDepthStencilValue(250);
+    }
+    if (Weapon)
+    {
+        Weapon->SetRenderCustomDepth(true);
+        Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_STENCIL_RED);
+    }
 }
 
 void AAuraEnemyCharacter::UnhighlightActor()
 {
-    GetCapsuleComponent()->SetHiddenInGame(true);
+    if (USkeletalMeshComponent* MyMesh = GetMesh())
+    {
+        MyMesh->SetRenderCustomDepth(false);
+    }
+    if (Weapon)
+    {
+        Weapon->SetRenderCustomDepth(false);
+    }
 }
