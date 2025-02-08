ad45d2ff0d629ab8ef43c2c1203f8062772efa5d
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-02-09 00:54:42 +0200

    added blueprint function library

---
+++ Source/Aura/Game/AuraBlueprintFunctionLibrary.cpp
@@ -0,0 +1,37 @@
+// Copyright 4sandwiches
+
+
+#include "Game/AuraBlueprintFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+#include "UI/HUD/AuraHUD.h"
+
+UOverlayWidgetController* UAuraBlueprintFunctionLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+    if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+    {
+        checkf(PC->IsLocalController(), TEXT("The Player Controller must be a local player controller only."));
+
+        if (AAuraHUD* AuraHUD = PC->GetHUD<AAuraHUD>())
+        {
+            return AuraHUD->GetOverlayWidgetController();
+        }
+    }
+
+    return nullptr;
+}
+
+UAttributeMenuWidgetController* UAuraBlueprintFunctionLibrary::GetAttributeMenuWidgetController(const UObject* WorldContextObject)
+{
+    if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+    {
+        checkf(PC->IsLocalController(), TEXT("The Player Controller must be a local player controller only."));
+
+        if (AAuraHUD* AuraHUD = PC->GetHUD<AAuraHUD>())
+        {
+            return AuraHUD->GetAttributeMenuWidgetController();
+        }
+    }
+
+    return nullptr;
+}
---
+++ Source/Aura/Game/AuraBlueprintFunctionLibrary.h
@@ -0,0 +1,24 @@
+// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraBlueprintFunctionLibrary.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UAuraBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintPure, Category = "AuraAbilitySystem|WidgetController", BlueprintCosmetic, meta = (WorldContext = "WorldContextObject"))
+    static class UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+
+    UFUNCTION(BlueprintPure, Category = "AuraAbilitySystem|WidgetController", BlueprintCosmetic, meta = (WorldContext = "WorldContextObject"))
+    static class UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const UObject* WorldContextObject);
+};
