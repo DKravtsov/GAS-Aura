18eeea6754bf5e34449f746475fd30867070a58c
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-02-10 20:23:24 +0200

    facing the target using motion warping

--- Source/Aura/Characters/CombatInterface.h
+++ Source/Aura/Characters/CombatInterface.h
@@ -25,4 +25,8 @@ public:
     virtual int32 GetCharacterLevel() const { return 0; }
 
     virtual FVector GetCombatSocketLocation() const { return {}; }
+
+    // Set Target Location for anim montages with MotionWarping or similar
+    UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+    void UpdateFacingTarget(const FVector& TargetLocation);
 };
