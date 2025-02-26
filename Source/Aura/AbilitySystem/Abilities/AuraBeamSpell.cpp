dc22012cc1fe584a740ab72f7b7b4b3079a47ec1
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-02-26 21:38:42 +0200

    Electrocute's animation setup

--- Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -3,3 +3,50 @@
 
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UAuraBeamSpell::StoreDataFromHitResult(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		CursorHitLocation = HitResult.ImpactPoint;
+		CursorHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+}
+
+void UAuraBeamSpell::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnAvatarSet(ActorInfo, Spec);
+
+	if (ActorInfo)
+	{
+		OwningPlayerController = ActorInfo->PlayerController.Get();
+	}
+}
+
+void UAuraBeamSpell::DisableMovement() const
+{
+	if (const ACharacter* AvatarCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo()))
+	{
+		AvatarCharacter->GetCharacterMovement()->DisableMovement();
+	}
+}
+
+void UAuraBeamSpell::EnableMovement() const
+{
+	if (const ACharacter* AvatarCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo()))
+	{
+		AvatarCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+	}
+}
--- Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -13,5 +13,31 @@ UCLASS()
 class AURA_API UAuraBeamSpell : public UDamageGameplayAbility
 {
 	GENERATED_BODY()
-	
+public:
+
+protected:
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector CursorHitLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> CursorHitActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwningPlayerController;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void StoreDataFromHitResult(const FHitResult& HitResult);
+
+	UFUNCTION(BlueprintCallable)
+	void DisableMovement() const;
+
+	UFUNCTION(BlueprintCallable)
+	void EnableMovement() const;
+
+    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
 };
