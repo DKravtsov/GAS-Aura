81f17d4dffe198bc0f916131cc52b1015bbc9696
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-02-27 15:44:53 +0200

    Electrocute: finished

--- Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -3,6 +3,7 @@
 
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
+#include "Characters/CombatInterface.h"
 #include "GameFramework/Character.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
@@ -11,7 +12,7 @@ void UAuraBeamSpell::StoreDataFromHitResult(const FHitResult& HitResult)
 	if (HitResult.bBlockingHit)
 	{
 		CursorHitLocation = HitResult.ImpactPoint;
-		CursorHitActor = HitResult.GetActor();
+		SetPrimaryTargetActor(HitResult.GetActor());
 	}
 	else
 	{
@@ -35,6 +36,19 @@ void UAuraBeamSpell::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, con
 	}
 }
 
+void UAuraBeamSpell::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	// Clear callbacks
+	SetPrimaryTargetActor(nullptr);
+	for (const auto& Target : SecondaryTargets)
+	{
+		UnbindTargetCallback(Target);
+	}
+}
+
 void UAuraBeamSpell::DisableMovement() const
 {
 	if (const ACharacter* AvatarCharacter = Cast<ACharacter>(GetAvatarActorFromActorInfo()))
@@ -50,3 +64,52 @@ void UAuraBeamSpell::EnableMovement() const
 		AvatarCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
 	}
 }
+
+int32 UAuraBeamSpell::GetNumSecondaryTargets() const
+{
+	return FMath::Min(GetAbilityLevel() - 1, MaxNumSecondaryTargets);
+}
+
+void UAuraBeamSpell::SetPrimaryTargetActor(AActor* NewPrimaryTarget)
+{
+	if (NewPrimaryTarget == PrimaryTargetActor)
+	{
+		return;
+	}
+	// Just in case, because we can change the primary target during ability execution
+	UnbindTargetCallback(PrimaryTargetActor);
+	PrimaryTargetActor = NewPrimaryTarget;
+	BindTargetCallback(PrimaryTargetActor);
+}
+
+void UAuraBeamSpell::SetSecondaryTargets(const TArray<AActor*>& NewSecondaryTargets)
+{
+	//Note: We don't need to unbind callbacks for secondary targets because we unlikely will change them during
+	//	the ability is processing. And we clear them up when the ability ends.
+	SecondaryTargets = NewSecondaryTargets;
+
+	for (auto Target : SecondaryTargets)
+	{
+		BindTargetCallback(Target);
+	}
+}
+
+void UAuraBeamSpell::BindTargetCallback(const TScriptInterface<ICombatInterface>& CombatInterface)
+{
+	if (auto CI = CombatInterface.GetInterface())
+	{
+		if (!CI->GetOnDeathDelegate().IsAlreadyBound(this, &UAuraBeamSpell::OnTargetDied))
+		{
+			CI->GetOnDeathDelegate().AddDynamic(this, &UAuraBeamSpell::OnTargetDied);
+		}
+	}
+}
+
+void UAuraBeamSpell::UnbindTargetCallback(const TScriptInterface<ICombatInterface>& CombatInterface)
+{
+	if (ICombatInterface* CI = Cast<ICombatInterface>(PrimaryTargetActor))
+	{
+		CI->GetOnDeathDelegate().RemoveDynamic(this, &UAuraBeamSpell::OnTargetDied);
+	}
+}
+
--- Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/Abilities/DamageGameplayAbility.h"
+#include "Characters/CombatInterface.h"
 #include "AuraBeamSpell.generated.h"
 
 /**
@@ -20,11 +21,17 @@ protected:
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
 	FVector CursorHitLocation;
 
-	UPROPERTY(BlueprintReadWrite, Category = "Beam")
-	TObjectPtr<AActor> CursorHitActor;
+	UPROPERTY(BlueprintReadWrite, Category = "Beam", BlueprintSetter=SetPrimaryTargetActor)
+	TObjectPtr<AActor> PrimaryTargetActor;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
 	TObjectPtr<APlayerController> OwningPlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Beam")
+	TArray<TObjectPtr<AActor>> SecondaryTargets;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 MaxNumSecondaryTargets = 5;
 
 public:
 
@@ -37,7 +44,27 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void EnableMovement() const;
 
-    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	UFUNCTION(BlueprintCallable)
+	void SetPrimaryTargetActor(AActor* NewPrimaryTarget);
 
+	UFUNCTION(BlueprintCallable)
+	void SetSecondaryTargets(const TArray<AActor*>& NewSecondaryTargets);
+
+	UFUNCTION(BlueprintCallable)
+	virtual int32 GetNumSecondaryTargets() const;
+
+	//~ Begin of UGameplayAbility interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
 	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~ End of UGameplayAbility interface
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTargetDied(AActor* DeadActor);
+
+private:
+
+	void BindTargetCallback(const TScriptInterface<ICombatInterface>& CombatInterface);
+	void UnbindTargetCallback(const TScriptInterface<ICombatInterface>& CombatInterface);
 };
