15635192e7482e912e8b59467d9fc88395a234a6
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-03-02 20:56:13 +0200

    checkpoint player start

---
+++ Source/Aura/Game/AuraCheckpoint.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright 4sandwiches
+
+
+#include "AuraCheckpoint.h"
+
+#include "Components/SphereComponent.h"
+
+
+AAuraCheckpoint::AAuraCheckpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CheckpointSphere = CreateDefaultSubobject<USphereComponent>("CheckpointSphere");
+	CheckpointSphere->SetupAttachment(GetRootComponent());
+	CheckpointSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CheckpointSphere->SetCollisionObjectType(ECC_WorldStatic);
+	CheckpointSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CheckpointSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionProfileName(FName("BlockAll"));
+}
+
+void AAuraCheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CheckpointSphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraCheckpoint::OnSphereOverlap);
+}
+
+void AAuraCheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		CheckpointSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		CheckpointReached(OtherActor);
+	}
+}
+
---
+++ Source/Aura/Game/AuraCheckpoint.h
@@ -0,0 +1,31 @@
+﻿// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "AuraCheckpoint.generated.h"
+
+UCLASS()
+class AURA_API AAuraCheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess="true"))
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+ 	TObjectPtr<class USphereComponent> CheckpointSphere;
+	
+public:
+	AAuraCheckpoint(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(AActor* Actor);
+};
