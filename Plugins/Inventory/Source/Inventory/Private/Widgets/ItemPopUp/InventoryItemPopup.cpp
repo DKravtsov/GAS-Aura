ad94d79a268557d19b16ca65070990a5916f0e37
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-06-24 14:04:44 +0300

    [inventory] added item pop up menu placeholder (wip)

---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/InventoryItemPopup.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright 4sandwiches
+
+
+#include "InventoryItemPopup.h"
+
+#include "Components/Button.h"
+#include "Components/Slider.h"
+
+void UInventoryItemPopup::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	Button_Split->OnClicked.AddDynamic(this, &UInventoryItemPopup::SplitButtonClicked);
+	Button_Consume->OnClicked.AddDynamic(this, &UInventoryItemPopup::ConsumeButtonClicked);
+	Button_Drop->OnClicked.AddDynamic(this, &UInventoryItemPopup::DropButtonClicked);
+	Slider_Split->OnValueChanged.AddDynamic(this, &UInventoryItemPopup::SliderValueChanged);
+}
+
+void UInventoryItemPopup::SliderValueChanged(float Value)
+{
+}
+
+void UInventoryItemPopup::SplitButtonClicked()
+{
+	if (OnSplitDelegate.ExecuteIfBound(GetSplitAmount(), GridIndex))
+	{
+		HideMenu();
+	}
+}
+
+void UInventoryItemPopup::DropButtonClicked()
+{
+	if (OnDropDelegate.ExecuteIfBound(GridIndex))
+	{
+		HideMenu();
+	}
+}
+
+void UInventoryItemPopup::ConsumeButtonClicked()
+{
+	if (OnConsumeDelegate.ExecuteIfBound(GridIndex))
+	{
+		HideMenu();
+	}
+}
+
+int32 UInventoryItemPopup::GetSplitAmount() const
+{
+	return FMath::FloorToInt32(Slider_Split->GetValue());
+}
+
+void UInventoryItemPopup::HideMenu()
+{
+	RemoveFromParent();
+}
---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemPopUp/InventoryItemPopup.h
@@ -0,0 +1,76 @@
+﻿// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryItemPopup.generated.h"
+
+class UButton;
+
+DECLARE_DELEGATE_TwoParams(FInventoryItemPopupMenuSplitDelegate, const int32 /*SplitAmount*/, const int32 /*GridIndex*/);
+DECLARE_DELEGATE_OneParam(FInventoryItemPopupMenuDropDelegate, const int32 /*GridIndex*/);
+DECLARE_DELEGATE_OneParam(FInventoryItemPopupMenuConsumeDelegate, const int32 /*GridIndex*/);
+
+/**
+ * The item popup widget when a user clicks RMB on the item
+ */
+UCLASS()
+class INVENTORY_API UInventoryItemPopup : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	FInventoryItemPopupMenuSplitDelegate OnSplitDelegate;
+	FInventoryItemPopupMenuDropDelegate OnDropDelegate;
+	FInventoryItemPopupMenuConsumeDelegate OnConsumeDelegate;
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Split;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Drop;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consume;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class USlider> Slider_Split;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> Text_SliderAmount;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class USizeBox> SizeBox_Root;
+
+	int32 GridIndex = INDEX_NONE;
+
+public:
+
+	//~ Begin of UUserWidget interface
+	virtual void NativeOnInitialized() override;
+	//~ End of UUserWidget interface
+
+	int32 GetSplitAmount() const;
+	void HideMenu();
+
+	int32 GetGridIndex() const {return GridIndex;}
+	void SetGridIndex(int32 NewValue) {GridIndex = NewValue;}
+
+private:
+
+	UFUNCTION()
+	void SliderValueChanged(float Value);
+	
+	UFUNCTION()
+	void SplitButtonClicked();
+
+	UFUNCTION()
+	void DropButtonClicked();
+
+	UFUNCTION()
+	void ConsumeButtonClicked();
+};
