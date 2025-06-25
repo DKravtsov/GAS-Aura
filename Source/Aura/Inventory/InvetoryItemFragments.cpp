f85d79dd0df3848273a8fb8b0db48e88d2c79fb0
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-06-25 19:33:14 +0300

    [Aura] potion fragment; Health and Mana potions

---
+++ Source/Aura/Inventory/InvetoryItemFragments.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright 4sandwiches
+
+
+#include "InvetoryItemFragments.h"
+
+#include "GameplayEffect.h"
+#include "Characters/AuraCharacterBase.h"
+
+void FAuraPotionInventoryFragment::OnConsume(const APlayerController* PC)
+{
+	if (PotionEffectClass == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s is trying to consume an item with AuraPotionInventoryFragment [%s] where PotionEffectClass is not specified."),
+			*GetNameSafe(PC), *GetFragmentTag().ToString());
+		return;
+	}
+
+	if (AAuraCharacterBase* Character = IsValid(PC) ? Cast<AAuraCharacterBase>(PC->GetCharacter()) : nullptr; IsValid(Character))
+	{
+		Character->ApplyGameplayEffectToSelf(PotionEffectClass, PotionLevel);
+	}
+}
---
+++ Source/Aura/Inventory/InvetoryItemFragments.h
@@ -0,0 +1,22 @@
+﻿// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Fragments/InventoryItemFragment.h"
+#include "UObject/Object.h"
+#include "InvetoryItemFragments.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraPotionInventoryFragment: public FInventoryItemConsumableFragment
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	TSubclassOf<class UGameplayEffect> PotionEffectClass;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	float PotionLevel;
+
+	virtual void OnConsume(const APlayerController* PC) override;
+};
