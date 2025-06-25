7c5e6f702417f1f1d244a3fc94d4e6a74d30d3cc
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-06-25 21:01:48 +0300

    [inventory] item description widget

---
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/ItemDescription/InventoryItemDescription.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright 4sandwiches
+
+
+#include "Widgets/ItemDescription/InventoryItemDescription.h"
+
+#include "Components/SizeBox.h"
+
+FVector2D UInventoryItemDescription::GetBoxSize() const
+{
+	return SizeBox_Root->GetDesiredSize();
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/ItemDescription/InventoryItemDescription.h
@@ -0,0 +1,23 @@
+﻿// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryItemDescription.generated.h"
+
+/**
+ * 
+ */
+UCLASS(MinimalAPI, Abstract)
+class UInventoryItemDescription : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class USizeBox> SizeBox_Root;
+
+public:
+
+	FVector2D GetBoxSize() const;
+};
