1142290fa1882e1f54ac7983cb293e422688ae29
DKravtsov <32927424+DKravtsov@users.noreply.github.com>
2025-07-02 00:45:50 +0300

    [inventory] highlightable skeletal mesh;
    plus updated material

---
+++ Plugins/Inventory/Source/Inventory/Private/Items/Components/InventoryHighlightableSkeletalMesh.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright 4sandwiches
+
+
+#include "Items/Components/InventoryHighlightableSkeletalMesh.h"
+
+void UInventoryHighlightableSkeletalMesh::Highlight_Implementation()
+{
+	SetOverlayMaterial(HighlightMaterial);
+}
+
+void UInventoryHighlightableSkeletalMesh::UnHighlight_Implementation()
+{
+	SetOverlayMaterial(nullptr);
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Items/Components/InventoryHighlightableSkeletalMesh.h
@@ -0,0 +1,27 @@
+﻿// Copyright 4sandwiches
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Interfaces/InventoryHighlightableInterface.h"
+#include "InventoryHighlightableSkeletalMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS(MinimalAPI)
+class UInventoryHighlightableSkeletalMesh : public USkeletalMeshComponent, public IInventoryHighlightableInterface
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TObjectPtr<UMaterialInterface> HighlightMaterial;
+
+public:
+
+	//~ Begin of IInventoryHighlightableInterface interface
+	INVENTORY_API virtual void Highlight_Implementation() override;
+	INVENTORY_API virtual void UnHighlight_Implementation() override;
+	//~ End of IInventoryHighlightableInterface interface
+};
